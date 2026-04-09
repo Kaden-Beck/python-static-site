6bfea7250d4dccc86234fe1b8635406555f7ee52
Kaden-Beck <kadenb12@gmail.com>
2026-04-09 15:48:27 -0600

    Add initial project structure with main script and text node implementation
    - Create .gitignore to exclude __pycache__ directory
    - Add README.md for project documentation
    - Implement main.sh to run the main script
    - Define TextNode class and TextType enum in textnode.py
    - Create main.py to demonstrate TextNode usage

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode
+
+
+def main():
+    node = TextNode("This is ome anchor text", "link", "https://www.boot.dev")
+
+    print(node)
+    print("")
+
+
+if __name__ == "__main__":
+    main()
