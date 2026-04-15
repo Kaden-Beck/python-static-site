248d66269b804f56eae3588444fec3bc1db5d27e
Kaden-Beck <kadenb12@gmail.com>
2026-04-15 14:46:42 -0600

    Added additional content and move content directory

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.build import init_directory, generate_page
 
 def main():
     init_directory("./static", "./public")
-    generate_page("src/content/index.md", "src/template.html", "./public/index.html")
+    generate_page("./content/index.md", "src/template.html", "./public/index.html")
 
 
 if __name__ == "__main__":
