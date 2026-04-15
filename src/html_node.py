9be931b1791aabe3e3006373dbabcec0be39dee5
Kaden-Beck <kadenb12@gmail.com>
2026-04-15 10:33:58 -0600

    Wrote out plan for the markdown_to_html function implementation

--- src/html_node.py
+++ src/html_node.py
@@ -21,3 +21,17 @@ class HTMLNode:
 
     def __repr__(self):
         return f'<{self.tag or "no tag"}{self.props_to_html()}>{self.value or "no value"}</{self.tag or "no tag"}> Children: {self.children or "no children"}'
+
+
+def markdown_to_html_node(markdown: str) -> HTMLNode:
+    # 1) Spilt into blocks
+    # 2) For each block:
+    #   - Determine BlockType
+    #   - Based on block type, create a new HTMLNode
+    #   - Parse (TextNode -> HTMLNode) and assign child HTML Nodes (recursive) to block (Create a text_to_children(str) -> list[HTMLNode])
+    #   - For the code block : no inline parsing for children
+    # 3) Add each of these parent HTMLNodes with children to one singular parent node which is a <div> element
+    # 4) Add Unit Tests
+    
+    
+    pass
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from textnode import TextNode
 
-
 def main():
     node = TextNode("This is ome anchor text", "link", "https://www.boot.dev")
 
     print(node)
     print("")
+
+
+
 
 
 if __name__ == "__main__":
